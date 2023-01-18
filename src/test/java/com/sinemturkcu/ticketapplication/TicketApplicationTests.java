b2dd4e45b17e1d5a74b565f30fec805e0783f96e
sinemturkcu <sinemturkcu@gmail.com>
2023-01-18 22:05:50 +0300

    Initial commit

---
+++ src/test/java/com/sinemturkcu/ticketapplication/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinemturkcu.ticketapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
