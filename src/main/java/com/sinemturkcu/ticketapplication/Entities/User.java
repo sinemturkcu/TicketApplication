75d3901dbc0449d985026c97572968f210fe8cb3
sinemturkcu <sinemturkcu@gmail.com>
2023-01-18 23:15:51 +0300

    For id; generation strategy added.

--- src/main/java/com/sinemturkcu/ticketapplication/Entities/User.java
+++ src/main/java/com/sinemturkcu/ticketapplication/Entities/User.java
@@ -9,6 +9,7 @@ import java.util.Set;
 @Data
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String password;
