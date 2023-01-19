4f5ed97ed581acca512559df498c80cc7ebd168d
sinemturkcu <sinemturkcu@gmail.com>
2023-01-19 05:12:02 +0300

    Create UserRepository.java

---
+++ src/main/java/com/sinemturkcu/ticketapplication/Repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sinemturkcu.ticketapplication.Repositories;
+
+import com.sinemturkcu.ticketapplication.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u WHERE u.userName = :userName")
+    public User getUserByUsername(@Param("username") String userName);
+}
