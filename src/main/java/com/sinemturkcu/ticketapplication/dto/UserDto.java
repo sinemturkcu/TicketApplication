132206af33b952b80c2dec0f5f6a2a1259f7456e
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-20 21:07:07 +0300

    Dtos added

---
+++ src/main/java/com/sinemturkcu/ticketapplication/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.sinemturkcu.ticketapplication.dto;
+
+
+import com.sinemturkcu.ticketapplication.Entities.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private Role role;
+
+}
---
+++ src/main/java/com/sinemturkcu/ticketapplication/dto/UserDtoConverter.java
@@ -0,0 +1,23 @@
+package com.sinemturkcu.ticketapplication.dto;
+
+import com.sinemturkcu.ticketapplication.Entities.Role;
+import com.sinemturkcu.ticketapplication.Entities.User;
+
+public class UserDtoConverter {
+    public static UserDto convertToUserDto(User user){
+        if (user.getRole() == Role.ADMIN){
+            return UserDto.builder()
+                    .id(user.getId())
+                    .username(user.getUsername())
+                    .email(user.getEmail())
+                    .role(user.getRole()).build();
+        }
+        else{
+            return UserDto.builder()
+                    .id(user.getId())
+                    .username(user.getUsername())
+                    .email(user.getEmail())
+                    .role(user.getRole()).build();
+        }
+    }
+}
