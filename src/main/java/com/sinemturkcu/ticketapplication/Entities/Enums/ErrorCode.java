6a7136267653babbd574db0e0ecc127da72b7223
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-20 19:28:15 +0300

    ErrorCode and EventStatus enums added

---
+++ src/main/java/com/sinemturkcu/ticketapplication/Entities/Enums/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.sinemturkcu.ticketapplication.Entities.Enums;
+
+public enum ErrorCode {
+    BAD_REQUEST, USER_NOT_FOUND
+}
---
+++ src/main/java/com/sinemturkcu/ticketapplication/Entities/Enums/EventStatus.java
@@ -0,0 +1,7 @@
+package com.sinemturkcu.ticketapplication.Entities.Enums;
+
+public enum EventStatus {
+    TICKET_STATUS_CANCELED,
+    TICKET_STATUS_PURCHASED,
+    TICKET_STATUS_DELAYED,
+}
