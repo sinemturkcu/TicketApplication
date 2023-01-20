01fb314b6558250ebfcfc46fa04d416cb70db214
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-21 01:11:55 +0300

    Update RouteRepository.java

--- src/main/java/com/sinemturkcu/ticketapplication/Repositories/RouteRepository.java
+++ src/main/java/com/sinemturkcu/ticketapplication/Repositories/RouteRepository.java
@@ -2,6 +2,8 @@ package com.sinemturkcu.ticketapplication.Repositories;
 
 import com.sinemturkcu.ticketapplication.Entities.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RouteRepository extends JpaRepository<Route,Long> {
 }
