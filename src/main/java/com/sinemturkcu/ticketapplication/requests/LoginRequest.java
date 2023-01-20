c4168f55ba1d42056310b3abfff52ca36df35a3b
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-20 21:06:07 +0300

    Login, Register and UpdateUserRole requests added.

---
+++ src/main/java/com/sinemturkcu/ticketapplication/requests/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.sinemturkcu.ticketapplication.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
