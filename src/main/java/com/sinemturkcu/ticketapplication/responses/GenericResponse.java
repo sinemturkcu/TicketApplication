2eca1b77fd1732ca4c264248004be34dafdd815c
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-24 02:30:05 +0300

    Create GenericResponse.java

---
+++ src/main/java/com/sinemturkcu/ticketapplication/responses/GenericResponse.java
@@ -0,0 +1,11 @@
+package com.sinemturkcu.ticketapplication.responses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GenericResponse {
+    private String message;
+}
