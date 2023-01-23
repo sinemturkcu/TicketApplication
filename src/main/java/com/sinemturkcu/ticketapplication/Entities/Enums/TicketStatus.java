d0151b33fa999eec6867b8691e03df91e108e701
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-24 00:41:28 +0300

    Create TicketStatus.java

---
+++ src/main/java/com/sinemturkcu/ticketapplication/Entities/Enums/TicketStatus.java
@@ -0,0 +1,8 @@
+package com.sinemturkcu.ticketapplication.Entities.Enums;
+
+public enum TicketStatus {
+    TICKET_STATUS_CANCELED,
+    TICKET_STATUS_PURCHASED,
+    TICKET_STATUS_DELAYED,
+    TICKET_STATUS_REZERVATION,
+}
