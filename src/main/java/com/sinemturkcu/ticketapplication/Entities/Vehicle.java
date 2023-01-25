bd7c67a3e19c25247ba4f4edeb2211688fc9d8ab
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-25 19:18:31 +0300

    Update Vehicle.java

--- src/main/java/com/sinemturkcu/ticketapplication/Entities/Vehicle.java
+++ src/main/java/com/sinemturkcu/ticketapplication/Entities/Vehicle.java
@@ -1,26 +1,33 @@
 package com.sinemturkcu.ticketapplication.Entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
+@Table(name = "vehicles")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
-    private String serialNumber;
-    private String vehicleCompany;
-    private String seatNumber;
-    private String restSeat;
+    private int capacity;
+    private double seatPrice;
+    private String departureTime;
+    private String departureCity;
+    private String destinationCity;
 
-    /*
-    @ManyToOne
-    @JoinColumn(name = "route_id")
-    private Route route;
-     */
+    @JsonIgnore
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL)
+    private List<Ticket> tickets;
+
+
 
 }
