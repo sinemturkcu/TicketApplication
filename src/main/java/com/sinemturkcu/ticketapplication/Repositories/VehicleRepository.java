f38c42d4c048146e4ff12b14c4757e9ae896a088
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-25 19:30:45 +0300

    findVehicleByDepartureCityAndDestinationCityAndDepartureTime method added.

--- src/main/java/com/sinemturkcu/ticketapplication/Repositories/VehicleRepository.java
+++ src/main/java/com/sinemturkcu/ticketapplication/Repositories/VehicleRepository.java
@@ -2,9 +2,11 @@ package com.sinemturkcu.ticketapplication.Repositories;
 
 import com.sinemturkcu.ticketapplication.Entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+    List<Vehicle> findVehicleByDepartureCityAndDestinationCityAndDepartureTime(String departureCity, String destinationCity, String departureTime);
 }
