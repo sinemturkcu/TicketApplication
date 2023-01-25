b860ad2255282afc30ee321c07e4095bb9e9e10d
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-25 19:18:28 +0300

    Update Ticket.java

--- src/main/java/com/sinemturkcu/ticketapplication/Entities/Ticket.java
+++ src/main/java/com/sinemturkcu/ticketapplication/Entities/Ticket.java
@@ -1,28 +1,60 @@
 package com.sinemturkcu.ticketapplication.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.sinemturkcu.ticketapplication.Entities.Enums.TicketStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import java.util.List;
-import java.util.Set;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Ticket {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private double price;
-    private String departureTime;
-    private String arrivalTime;
+    private String id;
 
-    @OneToOne
-    Vehicle vehicle;
+    @Column(name = "ticket_number")
+    private int ticketNumber;
 
-    @OneToOne
-    Route route;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "bus_id")
+    private Vehicle vehicle;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private TicketStatus status;
+
+    @Column(name = "ticket_price")
+    private Double ticketPrice;
+
+    @Column(name = "ticket_date")
+    private String ticketDate;
+
+    @Column(name = "ticket_seat")
+    private int ticketSeat;
+
+    @Column(name = "bus_departure_time")
+    private String busDepartureTime;
+
+    @Column(name = "from_direction")
+    private String fromDirection;
+
+    @Column(name = "to_direction")
+    private String toDirection;
 
 
 
