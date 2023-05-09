39a02c1d1113d46001b37dad3dba465f40bf6870
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-05-09 10:25:40 +0300

    @EnableSwagger2 annotation add

--- src/main/java/com/sinemturkcu/ticketapplication/TicketApplication.java
+++ src/main/java/com/sinemturkcu/ticketapplication/TicketApplication.java
@@ -2,12 +2,16 @@ package com.sinemturkcu.ticketapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class TicketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketApplication.class, args);
     }
+
+
 
 }
